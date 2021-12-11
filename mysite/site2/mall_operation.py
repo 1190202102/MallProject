9afc436c8a77dd4991bbca8b638455ee6a40ed99
hahaha-hy <1735175988@qq.com>
2021-12-11 13:56:12 +0800

    aksjdhajdsh

--- mysite/site2/mall_operation.py
+++ mysite/site2/mall_operation.py
@@ -1,4 +1,36 @@
+import json
+from datetime import time
+import random
+import mysql.connector
+myconfig={
+    'host':'localhost',
+    'user':'root',
+    'pwd':'200178heyang'
+}
 def sendToBank(msg_toBank):
     pass
-def proceed_trade(shopcart,clientID):
-    pass
+def proceed_trade(shopcart_json,clientID):
+    shopcart=json.loads(shopcart_json)
+    db = mysql.connector.connect(
+        host=myconfig['host'],
+        user=myconfig['user'],
+        password=myconfig['pwd'],
+        db='online_mall',
+    )
+
+    cursor=db.cursor()
+    cursor.execute(f"select cart_info from cart where owner=\'{clientID}\'")
+    result=cursor.fetchall()
+    cart=result[0][0]
+
+    cursor.execute("select count(*) from product_list")
+    result = cursor.fetchall()
+    line = result[0][0]
+    lines=str(line+1)
+
+    trade_time= str(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
+    prog = 100 * random.random()
+
+    cursor.execute(f"insert into bought_prod (trade_id,client,goods,trade_time,progess) values(\'{lines}\',\'{clientID}\',\'{cart}\',\'{trade_time}\',\'{prog}\')")
+    db.commit()
+    return
--- mysite/site2/mydb.py
+++ mysite/site2/mydb.py
@@ -1,2 +1,22 @@
+import mysql.connector
+from . import crypto
+myconfig={
+    'host':'localhost',
+    'user':'root',
+    'pwd':'200178heyang'
+}
+CAHost='127.0.0.1'
 def get_pubkey_fromClientDB(clientID):
-    pass
+    db = mysql.connector.connect(
+        host=myconfig['host'],
+        user=myconfig['user'],
+        password=myconfig['pwd'],
+        db='bank_base',
+    )
+    cursor = db.cursor()
+
+    cursor.execute(f"select certi from cli_certi where client=\'{clientID}\'")
+    result = cursor.fetchall()
+    certi = result[0][0]
+    pubkey = crypto.load_cert(certi)['pubkey']
+    return pubkey
